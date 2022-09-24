cc8d3829ed05984325d142f61b9e40979e6f9b53
Limai <sasha.nafasov@gmail.com>
2022-09-24 23:38:56 +0500

    сохранение файла

---
+++ src/methods/saveFile.ts
@@ -0,0 +1,56 @@
+import {Request, Response} from 'express';
+
+import fetch from 'node-fetch';
+import path from 'path';
+import {createWriteStream} from 'fs';
+
+type FileData = {
+  filename: string
+  url: string
+}
+
+async function saveFile(req: Request, res: Response) {
+  if (!req.body) {
+    res.json({
+      status: false,
+      message: 'Не введены filename и url',
+    });
+  }
+
+  const {filename, url}: FileData = req.body;
+  if (!filename || !url) {
+    return res.json({
+      status: false,
+      message: 'Название файла и ссылка на файл не введены.',
+    });
+  }
+
+  const downloadPath = path.resolve(__dirname, '../../files/schedule', filename);
+  const writer = createWriteStream(downloadPath);
+
+  const response = await fetch(url, {
+    method: 'POST',
+  });
+
+  response.body!.pipe(writer);
+
+  writer.on('error', (error) => {
+    console.log('Ошибка при сохранении файла:', error);
+    return res.json({
+      status: false,
+      message: error.message,
+    });
+  });
+
+  writer.on('close', () => {
+    return res.json({
+      status: true,
+      message: 'Файл успешно сохранён',
+      filename,
+    });
+  });
+
+  writer.on('finish', writer.close);
+}
+
+export default saveFile;
--- src/routers/schedule.ts
+++ src/routers/schedule.ts
@@ -3,8 +3,10 @@ const router = express.Router();
 
 import getSchedule from '../methods/getSchedule';
 import parseSchedule from '../methods/parseSchedule';
+import saveFile from '../methods/saveFile';
 
 router.get('/get', getSchedule);
 router.get('/parse', parseSchedule);
+router.post('/saveFile', saveFile);
 
 export default router;
