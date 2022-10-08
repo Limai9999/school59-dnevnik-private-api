fd8fd84e1ea50074ee5201d37e91cb62b4c2c899
Limai <sasha.nafasov@gmail.com>
2022-10-08 23:51:26 +0500

    добавлены Utils

--- src/types/methods/RequestLocals.ts
+++ src/types/methods/RequestLocals.ts
@@ -1,9 +1,11 @@
 import {Application, Request} from 'express';
 
 import NetCitySession from '../../modules/NetCitySession';
+import Utils from '../../modules/Utils';
 
 export type AppLocals = {
   netcitySession: NetCitySession
+  utils: Utils
 }
 
 interface ApplicationLocals extends Application {
