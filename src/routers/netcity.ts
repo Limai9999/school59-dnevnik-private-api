6f963c719bd471166b1b59af9d88f5dd9f41dc71
Limai <sasha.nafasov@gmail.com>
2022-10-08 23:52:02 +0500

    добавлен downloadAttachment router и переименован метод

--- src/routers/netcity.ts
+++ src/routers/netcity.ts
@@ -3,10 +3,12 @@ const router = express.Router();
 
 import {MethodInputData} from '../types/methods/MethodInputData';
 
-import getCookies from '../methods/login/getCookies';
-import closeSession from '../methods/login/closeSession';
+import getCookies from '../methods/netcity/getCookies';
+import closeSession from '../methods/netcity/closeSession';
+import downloadAttachment from '../methods/netcity/downloadAttachment';
 
 router.get('/getCookies', (req, res) => getCookies({req, res} as MethodInputData));
 router.get('/closeSession', (req, res) => closeSession({req, res} as MethodInputData));
+router.post('/downloadAttachment', (req, res) => downloadAttachment({req, res} as MethodInputData));
 
 export default router;
